836782b5ea14f80f06aa9cf0fd330376fd0ae653
Mike Schwab <mike.schwab@gmail.com>
2015-11-19 20:12:52 -0500

    postgres configuration

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151119235127) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "logistics", force: :cascade do |t|
     t.string   "name"
     t.text     "details"
