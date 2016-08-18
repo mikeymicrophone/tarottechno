3aa658c2201e107255becb0253938b5afa8fdef1
Mike Schwab <mike.schwab@gmail.com>
2016-08-18 00:04:05 -0400

    set moniker during registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_reader!
   layout 'back_room'
+  
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :moniker
+    devise_parameter_sanitizer.for(:account_update) << :moniker
+  end
 end
