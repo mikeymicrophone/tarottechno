5d983d4de4b29a204e00a440a42c7d821eb9717e
Mike Schwab <mike.schwab@gmail.com>
2016-08-19 22:19:53 -0400

    interpret a card after appearing it

--- app/helpers/interpretations_helper.rb
+++ app/helpers/interpretations_helper.rb
@@ -1,2 +1,12 @@
 module InterpretationsHelper
+  def appearance_interpretation_form appearance
+    form_for appearance.interpretations.new, :remote => true, :html => {:id => dom_id(appearance, :interpretation_of)} do |interpretation_form|
+      interpretation_form.text_field(:phrase) +
+      interpretation_form.text_area(:description) +
+      interpretation_form.submit +
+      interpretation_form.hidden_field(:appearance_id, :value => appearance.id) +
+      interpretation_form.hidden_field(:card_id, :value => appearance.card_id) +
+      interpretation_form.hidden_field(:reader_id, :value => appearance.reader.id)
+    end
+  end
 end
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,7 +1,9 @@
 class Appearance < ActiveRecord::Base
   belongs_to :reading
+  has_one :reader, :through => :reading
   belongs_to :card
   belongs_to :position
+  has_many :interpretations
   
   def name
     "#{card.name} in #{position.name} during #{reading.name}"
