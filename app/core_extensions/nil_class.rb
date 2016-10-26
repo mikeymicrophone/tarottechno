6a688404a01e0d6a4f01d953fdcb88997323c646
Mike Schwab <mike.schwab@gmail.com>
2016-10-25 20:48:52 -0400

    nil.name

---
+++ app/core_extensions/nil_class.rb
@@ -0,0 +1,5 @@
+class NilClass
+  def name
+    ""
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require_relative '../app/core_extensions/nil_class.rb'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
