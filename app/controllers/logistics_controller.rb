113270b7958f2e4ad951cceabd0cc4f1e66c2643
Mike Schwab <mike.schwab@gmail.com>
2017-02-01 01:48:16 -0500

    guildlines for djs and readers

--- app/controllers/logistics_controller.rb
+++ app/controllers/logistics_controller.rb
@@ -1,4 +1,12 @@
 class LogisticsController < ApplicationController
   skip_before_action :authenticate_reader_or_querent
   layout 'front_page'
+  
+  def dj_guildlines
+    
+  end
+  
+  def reader_guildlines
+    
+  end
 end
